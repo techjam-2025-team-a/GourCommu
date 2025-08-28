8e41d8d8a4f451f281b1419b4af706892745a9bb
Yuuki Takusi <e215737@ie.u-ryukyu.ac.jp>
2025-08-28 19:41:10 +0900

    fix: prisma内のファイルのESLintエラーを除外

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,18 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
- 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
- 
+
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
- 
+
 const eslintConfig = [
+  {
+    ignores: ["lib/generated/prisma/**"],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   // 以下を追加
   {
@@ -18,10 +21,6 @@ const eslintConfig = [
       quotes: ["error", "double"],
     },
   },
-  {
-    ignores: ["lib/generated/**/*"],
-  },
 ];
- 
+
 export default eslintConfig;
- 
